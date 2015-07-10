3a8c32d0e3762395113084ca408bc0a1ab6b94d4
aewens <aewenscript@gmail.com>
2015-07-09 23:33:38 -0500

    If, when, do

--- src/sip_of_clojure/core.clj
+++ src/sip_of_clojure/core.clj
@@ -1,7 +1,21 @@
 (ns sip-of-clojure.core
-  (:gen-class))
+    (:gen-class))
+  
+(def four (+ 2 2))
+(def yes true)
+(def no false)
+
+(def res0 
+    (if yes
+        four
+        no))
 
 (defn -main
-  "the origin of this lovely program"
-  [& args]
-  (println (str "2 + 2 is " (+ 2 2))))
+    "the origin of this lovely program"
+    [& args]
+    (if (= res0 four)
+        (do (def res1 (str "2 + 2 is " four))
+            (println res1))
+        (when (not res0)
+            (def res1 "Nope")
+            (println res1))))
